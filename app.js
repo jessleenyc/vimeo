b7f3e537264c603ed0ed65b8a62d613dbbd5faa6
Jess Lee <jessleenyc@gmail.com>
2016-01-25 13:24:44 -0500

    .

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.set('view engine', 'ejs');
 var db;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-// var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/vimeo';
-var mongoUrl = 'mongodb://localhost:27017/vimeo';
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/vimeo';
 MongoClient.connect(mongoUrl, function(err, database) {
   if (err) { throw err; }
   db = database;
