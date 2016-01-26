f5a7b7ec868d048cbfd4a4752b2206149192f928
Jess Lee <jessleenyc@gmail.com>
2016-01-25 19:47:50 -0500

    updates css

--- public/controllers/register_controller.js
+++ public/controllers/register_controller.js
@@ -29,7 +29,7 @@ function RegisterController($http) {
     $http
       .post('/login', data)
       .then(function(response) {
-        
+        //after a user registers, they are immediately signed in. However, i want the index ejs to reflect the change--registration from should be removed and login should be replaced with logout. it will only render if i refresh
       })
   }
 
